f2f6a4e10f5b6fecac4e2e9ab9be354abc6bcc08
arsalan <arslaniftikhar012345@gmail.com>
2024-05-24 02:10:05 +0500

    day3 added new endpoints and now all done

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import pandas as pd
 
 app = Flask("__name__")
 
-station = pd.read_csv("data/stations.txt", skiprows=17)
-station=station[["STAID","STANAME                                 "]]
+stations = pd.read_csv("data/stations.txt", skiprows=17)
+stations = stations[["STAID", "STANAME                                 "]]
 
 
 @app.route("/")
 def home():
-    return render_template("home.html", data=station.to_html())
+    return render_template("home.html", data=stations.to_html())
 
 
 @app.route("/api/v1/<station>/<date>")
@@ -22,6 +22,23 @@ def about(station, date):
     return {"station": station,
             "date": date,
             "temperature": temperature}
+
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "data/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station, year):
+    filename = "data/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return result
 
 
 if __name__ == "__main__":
